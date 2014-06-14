a4bd2c11715dbc05721fb77954d7cd0fe125e59d
Arturo Lopez <arlopez@csumb.edu>
2014-06-14 13:17:34 -0700

    Commit before merge

--- registration/views.py
+++ registration/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+#def index(request):
+#return render(request, "registration/base.html")
+
+
 def index(request):
-    return render(request, "registration/base.html")
+    return render(request, "reporting/check.html")
 
 def registration(request):
     return render(request, "registration/register.html")
