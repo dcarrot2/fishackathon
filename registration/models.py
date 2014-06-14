fe68e87067fcf608dc640bff410c14ea8de38dbd
Daniel Diaz <diazjfdaniel@gmail.com>
2014-06-14 09:21:51 -0700

    Linked reporting url to base html

--- registration/models.py
+++ registration/models.py
@@ -8,7 +8,7 @@ class Manager(models.Model):
     occupation = models.CharField(max_length=200)
     phone_number = models.IntegerField()
     manager_address = models.CharField(max_length=200)
-    number_of_canoes = models.IntegerField()
+    number_of_canoes = models.IntegerField(default=0)
     
     hull_material = models.CharField(max_length=200)
     vessel_size = models.DecimalField(max_digits=10, decimal_places=3)
@@ -20,7 +20,7 @@ class Manager(models.Model):
 
 class Canoe(models.Model):
     owner = models.ForeignKey(Manager)
-    current_canoe_number = models.IntegerField()
+    current_canoe_number = models.IntegerField(default=0)
     coastal_region = models.CharField(max_length=100)
     coastal_district = models.CharField(max_length=100)
     fishing_village = models.CharField(max_length=100)
@@ -29,3 +29,14 @@ class Canoe(models.Model):
     type_of_ownership = models.CharField(max_length=100)
     name_canoe_owner = models.CharField(max_length=100)
     name_captain = models.CharField(max_length=100)
+    year_purchased = models.IntegerField(default=0)
+    year_built = models.IntegerField(default=0)
+    place_built = models.IntegerField(default=0)
+    first_year_fishing = models.IntegerField(default=0)
+    construction_material = models.CharField(max_length=50)
+    length_overall = models.IntegerField(default = 0)
+    type_storage = models.BooleanField() #True is ice, false is no ice
+    crew_number = models.IntegerField()
+    is_motorized = models.BooleanField() #True if motorized, false if not
+    model_of_engine
+    
