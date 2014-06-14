5388f247ea774b2e64b84d67e8a9976a8fb15465
Daniel Diaz <diazjfdaniel@gmail.com>
2014-06-14 01:07:30 -0700

    Added to installed apps

--- fishackathon/settings.py
+++ fishackathon/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fishackathon',
+    'licensing',
+    'registration',
+    'reporting',
 )
 
 MIDDLEWARE_CLASSES = (
