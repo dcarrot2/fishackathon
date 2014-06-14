183556ba628a35447758b6505a37d5c20e4c0b12
Daniel Diaz <diazjfdaniel@gmail.com>
2014-06-14 02:03:29 -0700

    With header implemented

--- fishackathon/settings.py
+++ fishackathon/settings.py
@@ -88,3 +88,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
